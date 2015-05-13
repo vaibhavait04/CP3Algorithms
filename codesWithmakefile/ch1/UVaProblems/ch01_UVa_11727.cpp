0b97911e67754e1b6fed803c3994d231c141f316
vaibhavait04 <vaibhavait04@gmail.com>
2015-05-13 03:10:51 +0100

    restructured the solutions

---
+++ codesWithmakefile/ch1/UVaProblems/ch01_UVa_11727.cpp
@@ -0,0 +1,23 @@
+#include <iostream> 
+#include <cmath> 
+#include <cstdio>
+#include <algorithm> 
+
+using namespace std; 
+
+int main()
+{
+int N; 
+cin >> N;
+int x =1;
+while (N--)
+{
+int es[3];
+cin >> es[0] >> es[1] >> es[2]; 
+//cout << " salaries read are " << es[0] << " and " << es[1] << " and " << es[2] << "\n"; 
+sort(es, es+3); /// SORT NEEDS PAST THE END ITERATOR 
+//cout << " salaries sorted are " << es[0] << " and " << es[1] << " and " << es[2] << "\n"; 
+cout << "Case "<< x << ": "<< es[1] << "\n";
+++x; 
+}
+}
